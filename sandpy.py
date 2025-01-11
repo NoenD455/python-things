11f0a1b0471ae21c51b3202cc79651e95b71620d
NoenD <107866288+NoenD455@users.noreply.github.com>
2025-01-11 16:31:46 +0300

    sandpy
    uhh kinda fixd x,y

--- sandpy.py
+++ sandpy.py
@@ -7,14 +7,19 @@ posy = 0
 posx = 0
 running = True
 col = 0
-boardr = [random.randint(0, 100) for _ in range(1024)]
-boardg = [random.randint(0, 100) for _ in range(1024)]
-boardb = [random.randint(0, 100) for _ in range(1024)]
+boardr = [random.randint(0, 100) for _ in range(1025)]
+boardg = [random.randint(0, 100) for _ in range(1025)]
+boardb = [random.randint(0, 100) for _ in range(1025)]
 read = 0
 cursorx = 0
 cursory = 0
 cursorposx = 0
 cursorposy = 0
+
+def bill(x,y) :
+    if x > y :
+        return x - y
+    
 
 while True :
     col = col%255 + 1
@@ -27,10 +32,10 @@ while True :
         read = 0    
     posx = posx + 15.625
     read = read + 1
-    if read > 1023 :
+    if read > 1024 :
         read = 0
-        boardg[round(cursory/32)+33] = 255
-        boardb[round(cursorx/32)+32] = 255
+        boardg[(round(cursory/32*2)*16*2+33)%1024] = 255
+        boardb[round(cursorx/32*2)+33] = 255
     cursorposx, cursorposy = pygame.mouse.get_pos()
     cursorx = round(cursorposx/16)*16
     cursory = round(cursorposy/16)*16
@@ -41,6 +46,7 @@ while True :
             running = False
     pygame.draw.rect(screen,(boardr[read],boardg[read],boardb[read]),pygame.Rect(posx-15.625,posy-15.625,16,16))
     pygame.display.flip()
-    print(round(cursory/32)+32)
+    print(bill(round(cursory/32*2)*16*2+33,1024))
+    print(bill(10,20))
 
     
