1271926b6e3da202b058f719a611e10925f7306c
NoenD <107866288+NoenD455@users.noreply.github.com>
2025-06-08 09:11:07 +0300

    game finished
    YOOOOO

--- collisions.py
+++ collisions.py
@@ -1,5 +1,8 @@
 import pygame
 import math
+import time
+wait = 0
+c = 1
 pygame.font.init()
 font = pygame.font.SysFont('Impact', 30)
 pygame.init()
@@ -26,6 +29,11 @@ reload = 3
 string = "READY " + str(round(reload, 2))
 time = 0
 mx, my = pygame.mouse.get_pos()
+angle_rad = math.atan2(vely, velx)
+larium = 0
+circle_x = x + 40 * math.cos(angle_rad)
+circle_y = y + 40 * math.sin(angle_rad)
+
 #   dont touch
 while running:
     for event in pygame.event.get():
@@ -35,13 +43,26 @@ while running:
     screen.fill((0,0,0))
     pygame.draw.circle(screen,(255,255,0),(x,y),40)
     pygame.draw.circle(screen,(255,0,255),(x1,y1),20)
+    
+    angle_rad = math.atan2(vely, velx)
+    circle_x = x + 40 * -math.cos(larium)
+    circle_y = y + 40 * -math.sin(larium)
+    pygame.draw.circle(screen,(255,0,0),(circle_x,circle_y),10)
+
+    pygame.draw.circle(screen,(255,32,64),(250,250),wait*100)
+
     text = font.render(string, 0, ((math.sin(time)*255)%255, (math.sin(time*2)*255)%255, (math.sin(time*3)*255)%255))
     csad = pygame.transform.scale(text,(abs(math.sqrt(abs(velx+velx1)/20)*10+256),abs(math.sqrt(abs(vely+vely1)/2)*10+32)))
     rectfont = text.get_rect()
     rectfont.topleft = (0, 0)
 
     screen.blit(csad, rectfont)
-
+    
+    if larium < angle_rad:
+        larium = larium + 0.0025
+    if larium > angle_rad:
+        larium = larium - 0.0025
+    
     pygame.display.update()
     x += velx/100
     y += vely/100
@@ -65,8 +86,22 @@ while running:
         
         y1 = y1 + tempy/100
         vely1 = tempy /1
-        
 
+    if euclidean_distance((circle_x,circle_y), (x1,y1))<=30: 
+        string = "game over"
+        c = 0
+    if c == 0:
+        if wait < 5:
+            wait = wait + 0.01
+            string = "game over"
+            print(wait)
+        string = "game over"
+        if wait > 4:
+            running = False
+            string = "game over"
+            running = False
+            print(3)
+        print(13)
     
     mx, my = pygame.mouse.get_pos()
     #   bound
@@ -130,10 +165,10 @@ while running:
     vely1 = vely1 / 1.001
     if reload < 3:
         reload = reload * 1.000125
-    if math.floor(reload) == 3:
+    if math.floor(reload) == 3 and wait == 0:
         string = "READY " + str(round(reload, 2))
-    elif not math.floor(reload) == 3:
+    elif not math.floor(reload) == 3 and wait == 0:
         string = "CHARGING " + str(round(reload, 2))
-
-
+    
+    
 
